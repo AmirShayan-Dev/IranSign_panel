d17bc8c34e85057c4606ddd5994b6849a58636ad
AmirShayanMobaraki <amirshayan1999@gmail.com>
2024-08-08 19:39:24 +0330

    Installed MUI in Project & tested!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Button } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<>Start Up the Project!</>);
+root.render(
+  <>
+    Start Up the Project!
+    <Button variant="contained">Test from mui</Button>
+  </>
+);
